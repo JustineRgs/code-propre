9188fe843e2a47e95fc099280ba4f02234380e79
Justine RAGUES <justineragues@gmail.com>
2024-07-30 11:51:53 +0200

    TP3

--- src/main/java/ex3/Zoo.java
+++ src/main/java/ex3/Zoo.java
@@ -6,31 +6,56 @@ public class Zoo {
 	private ZoneCarnivore zoneCarnivore;
 	private FermeReptile fermeReptile;
 	private Aquarium aquarium;
-	
+
+	// Constructeur du Zoo
 	public Zoo(String nom){
 		this.nom = nom;
+		this.savaneAfricaine = new SavaneAfricaine();
+		this.zoneCarnivore = new ZoneCarnivore();
+		this.fermeReptile = new FermeReptile();
+		this.aquarium = new Aquarium();
 	}
-	
+
+	// Méthode pour ajouter un animal
 	public void addAnimal(String nomAnimal, String typeAnimal, String comportement){
+		// Vérifier si les zones sont initialisées avant d'ajouter des animaux
 		if (typeAnimal.equals("MAMMIFERE") && comportement.equals("CARNIVORE")){
-			zoneCarnivore.addAnimal(typeAnimal, nomAnimal, comportement);
+			if (zoneCarnivore != null) {
+				zoneCarnivore.addAnimal(typeAnimal, nomAnimal, comportement);
+			}
 		}
 		else if (typeAnimal.equals("MAMMIFERE") && comportement.equals("HERBIVORE")){
-			savaneAfricaine.addAnimal(typeAnimal, nomAnimal, comportement);
+			if (savaneAfricaine != null) {
+				savaneAfricaine.addAnimal(typeAnimal, nomAnimal, comportement);
+			}
 		}
 		else if (typeAnimal.equals("REPTILE")){
-			fermeReptile.addAnimal(typeAnimal, nomAnimal, comportement);
+			if (fermeReptile != null) {
+				fermeReptile.addAnimal(typeAnimal, nomAnimal, comportement);
+			}
 		}
 		else if (typeAnimal.equals("POISSON")){
-			aquarium.addAnimal(typeAnimal, nomAnimal, comportement);
+			if (aquarium != null) {
+				aquarium.addAnimal(typeAnimal, nomAnimal, comportement);
+			}
 		}
 	}
-	
+
+	// Méthode pour afficher la liste des animaux
 	public void afficherListeAnimaux(){
-		savaneAfricaine.afficherListeAnimaux();
-		zoneCarnivore.afficherListeAnimaux();
-		fermeReptile.afficherListeAnimaux();
-		aquarium.afficherListeAnimaux();
+		// Vérifier si les zones sont initialisées avant d'afficher la liste des animaux
+		if (savaneAfricaine != null) {
+			savaneAfricaine.afficherListeAnimaux();
+		}
+		if (zoneCarnivore != null) {
+			zoneCarnivore.afficherListeAnimaux();
+		}
+		if (fermeReptile != null) {
+			fermeReptile.afficherListeAnimaux();
+		}
+		if (aquarium != null) {
+			aquarium.afficherListeAnimaux();
+		}
 	}
 
 	/** Getter for nom
