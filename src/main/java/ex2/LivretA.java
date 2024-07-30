b1dedf434e589f4e33030586c3ba6ef17ee56c78
Justine RAGUES <justineragues@gmail.com>
2024-07-30 11:32:42 +0200

    TP2

--- src/main/java/ex2/LivretA.java
+++ src/main/java/ex2/LivretA.java
@@ -1,9 +1,26 @@
 package ex2;
 
+/**
+ * Classe représentant un Livret A, héritant de CompteBancaire.
+ */
 public class LivretA extends CompteBancaire {
 
-	public LivretA(String type, double solde, double tauxRemuneration) {
-		super(type, solde, 0, tauxRemuneration);
+	/**
+	 * Constructeur pour un livret A.
+	 *
+	 * @param solde            Le solde initial du livret A
+	 * @param tauxRemuneration Le taux de rémunération du livret A
+	 */
+	public LivretA(double solde, double tauxRemuneration) {
+		super(solde, 0); // Découvert à 0 car non autorisé pour Livret A
+		this.type = "LA";
+		this.tauxRemuneration = tauxRemuneration;
 	}
 
+	/**
+	 * Applique la rémunération annuelle sur le solde du livret A.
+	 */
+	public void appliquerRemuAnnuelle() {
+		this.solde += this.solde * this.tauxRemuneration / 100;
+	}
 }
